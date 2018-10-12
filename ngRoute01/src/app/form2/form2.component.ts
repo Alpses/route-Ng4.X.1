e5b5fe1d629d0076601a5e319ff9b92ebd0ec326
Alpses <huyanzongbo@126.com>
2018-10-12 18:05:00 +0800

    https://segmentfault.com/a/1190000009037539

--- ngRoute01/src/app/form2/form2.component.ts
+++ ngRoute01/src/app/form2/form2.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface User {
+  name: string;
+  account: {
+    email: string;
+    confirm: string;
+  }
+}
+
 @Component({
   selector: 'app-form2',
   templateUrl: './form2.component.html',
   styleUrls: ['./form2.component.css']
 })
 export class Form2Component implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  private arr :string[];
+  private tips:string;
+  private tip1:string =`<input [ngModel]="user.name" (ngModelChange)="user.name = $event" [innerHTML]="htmlSnippet">`;
+  private tip2:string =`<input [(ngModel)]="user.name" [innerHTML]="htmlSnippet">`;
+  public user:User = {
+    name:'alpses',
+    account: {
+      email: 'alpses@cnooc.com',
+      confirm: 'alpses@cnooc.com'
+    }
+  }
+  private curContact={
+    firstname:'John',
+    lastname:'Fay',
+    street:'Hexiang',
+    zip:4000,
+    city:'GZ'
+  }
+  constructor() {
+    this.arr=['Template-Driven Forms - 模板驱动式表单','Reactive Forms - 响应式表单'];
+    this.tips="若使用 template-driven 表单，则在@NgModel中导入 FormsModule；若使用 reactive forms，则@NgModel导入 ReactiveFormsModule。"
   }
 
+  ngOnInit() {
+
+  }
+  onSbmit({value,valid}:{value:User,valid:boolean},ngsform){
+    console.log(value,valid);//对象解构
+    console.dir(ngsform)
+  }
 }
